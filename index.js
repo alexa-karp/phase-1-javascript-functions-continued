0a5a7383ff6928320455ac09cc31f80d639235df
Alexa Karp <alexakarp@hotmail.com>
2022-05-31 02:33:24 -0500

    commit tree e1c57acfc32e0346c0c909528f4a992acecc0c9d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+const defaultActivity = 'roller-skate';
+const myActivity = 'bathe my dog';
+
+function saturdayFun(activity) {
+    if (activity === myActivity) {
+        return `This Saturday, I want to ${myActivity}!`;
+    }
+    
+    else {
+        return `This Saturday, I want to ${defaultActivity}!`;
+    }
+}
+
+const mondayActivity = 'go to the office';
+const myMondayActivity = 'work from home';
+
+function mondayWork (activity) {
+    if (activity === myMondayActivity) {
+        return `This Monday, I will ${myMondayActivity}.`;
+    }
+    
+    else {
+        return `This Monday, I will ${mondayActivity}.`;
+    }
+}
+
+function wrapAdjective (flair = "*") {
+    return function (adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
